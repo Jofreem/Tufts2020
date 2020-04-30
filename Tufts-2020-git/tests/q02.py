23636afad416b52e9ba53a43b9c5af6bee04513e
Jack freeman <jfreem06@tufts.edu>
2020-04-29 23:06:13 -0400

    Fixing file source

---
+++ Tufts-2020-git/tests/q02.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> fooLead() #doctest: +SKIP
+          LeadingSpace
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
