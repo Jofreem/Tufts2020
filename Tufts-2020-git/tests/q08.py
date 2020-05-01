593d6015033c27cd274f9b44c956c32842f408e6
Jack Freeman <47823573+Jofreem@users.noreply.github.com>
2020-05-01 12:32:17 -0400

    Add files via upload
    ellipsis on dataframe

---
+++ Tufts-2020-git/tests/q08.py
@@ -0,0 +1,28 @@
+test = {
+  'name': 'Question 8',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> print(daf) #doctest: +ELLIPSIS
+                   0
+          0     Made
+          1       By
+          2     Jack
+          3  Freeman
+          4       in
+          5     20...
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': 'print("hi")',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
